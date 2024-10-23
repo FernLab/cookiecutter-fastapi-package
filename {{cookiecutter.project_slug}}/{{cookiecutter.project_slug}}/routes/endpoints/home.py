5ae105018ae0ef194dcc1f5bf9b157b69bdfca18
arashmad <arashmad.dev@gmail.com>
2024-10-23 14:14:37 +0200

    fixed style

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/routes/endpoints/home.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/routes/endpoints/home.py
@@ -5,25 +5,32 @@
 """
 
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 import {{ cookiecutter.project_slug }}.schemas.general as general_schema
 from {{ cookiecutter.project_slug }}.core.config_parser import config
 
 router = APIRouter()
 
-APP_NAME = config['meta']['app_name']
+APP_NAME = config["meta"]["app_name"]
 
 
-@router.get('/', tags=["Home"],
-            summary="Home Page",
-            response_model=general_schema.Message,
-            responses={
-                404: {"model": general_schema.Message},
-                500: {"model": general_schema.Message}})
+@router.get(
+    "/",
+    tags=["Home"],
+    summary="Home Page",
+    response_model=general_schema.Message,
+    responses={
+        404: {"model": general_schema.Message},
+        500: {"model": general_schema.Message},
+    },
+)
 def home():
     """Return HomePage."""
-    try:
-        return {'msg': f'{APP_NAME} APIs is working. Use /docs or /redoc to see the documentation.'}
-
-    except Exception as e:  # pragma: no cover
-        return {'msg': f'Unexpected Error => {e}'}
+    return JSONResponse(
+        status_code=200,
+        content={
+            "msg": f"{APP_NAME} APIs is working. "
+            "Use /docs or /redoc to see the documentation."
+        },
+    )
