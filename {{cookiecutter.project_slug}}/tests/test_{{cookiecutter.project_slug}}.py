40380727bee50fee80ce158fe92be92873167c86
Romulo <romulo@gfz-potsdam.de>
2024-11-08 12:46:02 +0100

    We only use pytest.

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -7,14 +7,8 @@
 
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 
-{% if cookiecutter.use_pytest == 'y' -%}
 import pytest
-{% else -%}
-import unittest
-{%- endif %}
-
 import {{ cookiecutter.project_slug }}
-{%- if cookiecutter.use_pytest == 'y' %}
 import os
 from fastapi.testclient import TestClient
 
@@ -77,28 +71,35 @@ class Test{{ cookiecutter.project_slug|title }}():
 @pytest.fixture
 def response():
     """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+    See more at: https://doc.pytest.org/en/latest/explanation/fixtures.html
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
 
 def test_content(response):
     """Sample pytest test function which prints the package version."""
     assert {{ cookiecutter.project_slug }}.__version__ == "{{ cookiecutter.version }}"
-{%- else %}
 
 
-class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
+class Test{{ cookiecutter.project_slug|title }}:
     """Tests for `{{ cookiecutter.project_slug }}` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+    @classmethod
+    def setup_class(cls):
+        """Run once for the entire class to set up any state."""
+        print("Setting up Test_{{ cookiecutter.project_slug|title }} class")
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+    @classmethod
+    def teardown_class(cls):
+        """Run once after all tests in the class have run."""
+        print("Tearing down Test_{{ cookiecutter.project_slug|title }} class")
+
+    def setup_method(self, method):
+        """Run before each test method to set up clean state."""
+        print(f"Setting up for {method.__name__}")
+
+    def teardown_method(self, method):
+        """Run after each test method to clean up."""
+        print(f"Tearing down {method.__name__}")
 
     def test_000_something(self):
         """Test something."""
-{%- endif %}
+        print({{ cookiecutter.project_slug }}.__version__)
